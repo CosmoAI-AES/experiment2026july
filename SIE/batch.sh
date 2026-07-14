ced5f3025e587c4399b8183eec35e0540a43c6f6
Hans Georg Schaathun <georg@schaathun.net>
2026-07-14 09:42:26 +0200

    Completed rerun of SIE

--- SIE/batch.sh
+++ SIE/batch.sh
@@ -10,7 +10,7 @@ then
   echo Logging to $log/$err
   time python -m CosmoSim --toml $D/dataset.toml --rnd \
          --csvfile $D/dataset.csv --outfile $D/roulette.csv  \
-         --directory $D/images -v  > $log 2> $err
+         --directory $D/images -vv  > $log 2> $err
   time python -m droulette.split $D/problem.toml
 fi 
 
